b099bc94271348f573673b87c153540c2d8d23b4
emreozcan3320 <emreozcan3320@gmail.com>
2018-12-06 23:20:44 +0300

    two user added hardcoded for login tests

--- ChatServer/src/com/muc/ServerWorker.java
+++ ChatServer/src/com/muc/ServerWorker.java
@@ -7,9 +7,12 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class ServerWorker extends Thread {
 
 	private Socket clientSocket;
+	private String login = null;
 
 	public ServerWorker(Socket clientSocket) {
 		this.clientSocket = clientSocket;
@@ -32,14 +35,44 @@ public class ServerWorker extends Thread {
 		
 		// we create a buffer reader to read line by line
 		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		outputStream.write("Program started ...\n".getBytes());
+		
 		String line;
 		while((line = reader.readLine()) != null) {
-			if("quit".equalsIgnoreCase(line)) {
-				break;
+			String[] tokens = StringUtils.split(line);
+			
+			if(tokens != null && tokens.length > 0 ) {
+				String cmd  = tokens[0];
+				if("quit".equalsIgnoreCase(cmd)) {
+					break;
+				}else if(cmd.equals("login")){
+					handleLogin(outputStream, tokens);
+				}else {
+					String msg = "unknown command :" + cmd +"\n";
+					outputStream.write(msg.getBytes());
+				}
 			}
-			String msg = "You typed : "+ line+"\n";
-			outputStream.write(msg.getBytes());
+			
 		}
 		clientSocket.close();
+	}
+
+	private void handleLogin(OutputStream outputStream, String[] tokens) throws IOException {
+		if(tokens.length == 3 ) {
+			String login = tokens[1];
+			String password = tokens[2];
+			
+			if(login.equals("guest")&&password.equals("guest")
+					|| (login.equals("emre")&&password.equals("emre"))) {
+				String msg = "ok login";
+				outputStream.write(msg.getBytes());
+				this.login = login;
+				System.out.println("User Loged In Succesfully : " + login);
+			}else {
+				String msg = "error login fail";
+				outputStream.write(msg.getBytes());
+			}
+		}
+		
 	}
 }
